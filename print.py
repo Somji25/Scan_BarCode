c3cdad605be77f093dd4cd9393aa17d821819025
Nawaphon_Luthapak <154293106+Somji25@users.noreply.github.com>
2025-07-01 15:11:25 +0700

    Update print.py

--- print.py
+++ print.py
@@ -95,4 +95,5 @@ def api_print_barcode():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
