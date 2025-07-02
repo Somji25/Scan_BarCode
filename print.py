8334039fb3972294cc65bd30959b7c00467a6c24
Nawaphon_Luthapak <154293106+Somji25@users.noreply.github.com>
2025-07-02 11:19:44 +0700

    Update print.py

--- print.py
+++ print.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-LOCAL_PC_API_URL = "http://local-pc-ip:port/print"  # เปลี่ยนเป็น IP และ port เครื่อง Local PC
+LOCAL_PC_API_URL = "http://127.0.0.1:6000/print"  # เปลี่ยนเป็น IP และ port เครื่อง Local PC
 
 @app.route('/print_barcode', methods=['POST'])
 def print_barcode():
